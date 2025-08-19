5eeb96ef1bc87dcdc1c0aa224123d3ea8a0a4615
Shahriar MS <shahriar.ms1k@gmail.com>
2025-08-19 11:07:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -14,6 +14,7 @@ import {
   DropdownMenuPortal,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
+  DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { useUser } from '@/hooks/use-user';
 import { useState } from 'react';
